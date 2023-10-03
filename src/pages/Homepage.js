ae0441ea2bfdcc3b74dc447c253c770b52b75f06
Jc Din <ozknightwalker62@gmail.com>
2023-10-03 21:01:56 +0800

    setup dummy value for site stats

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,13 +7,32 @@ import { BsPerson } from 'react-icons/bs';
 import { FiServer } from 'react-icons/fi';
 import { GoLocation } from 'react-icons/go';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import StatsCard from "../components/StatsCard";
 
 const HomePage = () => {
     const [activeUsers, setActiveUsers] = useState(0);
     const [issues, setIssues] = useState(0);
     const [marketItems, setMarketItems] = useState(0);
+
+    const randomizeNumber = (maxValue=100) => {
+        return Math.floor(Math.random() * maxValue);
+    }
+
+    useEffect(() => {
+        // randomize the values for now
+        setActiveUsers(randomizeNumber());
+        setIssues(randomizeNumber(200));
+        setMarketItems(randomizeNumber(300));
+
+        return () => {
+            setActiveUsers(0);
+            setIssues(0);
+            setMarketItems(0);
+        };  // cleanup function
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }
+    , []);
 
     return (
         <div>
