c96d1f80fe1ffaff732889ed2d1ed9ffea6d1646
Jc Din <ozknightwalker62@gmail.com>
2023-10-11 16:19:31 +0800

    update issues page to list issues and allow report issue, also provide report graph per issue

---
+++ src/components/IssueReportChart.js
@@ -0,0 +1,101 @@
+import {
+    CategoryScale,
+    Chart as ChartJS,
+    LineElement,
+    LinearScale,
+    PointElement,
+    Title,
+} from "chart.js";
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+
+import dayjs from "dayjs";
+import { issueReportGraphData } from "../db/issues";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title);
+
+const IssueReportsChart = ({ issue_id }) => {
+    const [reportData, setReportData] = useState({ labels: [], datasets: [] });
+    const [maxCounter, setMaxCounter] = useState(0);
+
+    const fetchReportsData = async () => {
+        const labels = [];
+        const data = [];
+
+        await issueReportGraphData(issue_id).then((datas) => {
+            datas
+                .sort((a, b) => a.reported_date - b.reported_date)
+                .forEach((item) => {
+                    labels.push(dayjs(item.reported_date.toDate()));
+                    data.push(item.counter);
+
+                    setMaxCounter(Math.max(item.counter + 2, maxCounter));
+                });
+        });
+
+        // if labels only contain less than 5 items then fill with the prev days
+        const label_length = labels.length;
+        if (label_length < 7) {
+            // acquire the first element and start from there
+            const first = labels[0] || dayjs();
+
+            // lacking days
+            for (let i = 0; i <= Math.abs(label_length - 7); i++) {
+                // Your code to be executed 7 times goes here
+                labels.unshift(first.subtract(i + 1, "day"));
+                data.unshift(0);
+            }
+        }
+
+        if (maxCounter === 0) {
+            setMaxCounter(2);
+        }
+
+        setReportData({
+            labels: labels.map((item) => item.format("DD/MM/YYYY")),
+            datasets: [
+                {
+                    label: "Reported Counts",
+                    data: data,
+                    fill: false,
+                    borderColor: "rgb(53, 162, 235)",
+                    backgroundColor: "rgba(255, 99, 132, 0.5)",
+                    borderWidth: 2,
+                },
+            ],
+        });
+    };
+
+    useEffect(() => {
+        fetchReportsData();
+    }, [issue_id]);
+
+    return (
+        <div>
+            <Line
+                data={reportData}
+                id={`issue-${issue_id}-chart`}
+                options={{
+                    scales: {
+                        y: {
+                            ticks: {
+                                precision: 0,
+                            },
+                            beginAtZero: true,
+                            max: maxCounter,
+                            stepSize: 1, // Set step size to 1 to show only whole numbers
+                        },
+                    },
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: "Issue Report History",
+                        },
+                    },
+                }}
+            />
+        </div>
+    );
+};
+
+export default IssueReportsChart;
--- src/db/issues.js
+++ src/db/issues.js
@@ -1,19 +1,104 @@
-
 import { db } from "../services/firebase";
 
-import { collection, onSnapshot, query, where } from "firebase/firestore";
+import {
+    addDoc,
+    collection,
+    doc,
+    getDocs,
+    onSnapshot,
+    orderBy,
+    query,
+    updateDoc,
+    where,
+} from "firebase/firestore";
 
 const IssueDBRef = collection(db, "Issues");
+const IssueReportDBRef = collection(db, "IssueReport");
 
 export const newIssueCount = () => {
     const lastWeekTimestamp = new Date();
     lastWeekTimestamp.setDate(lastWeekTimestamp.getDate() - 7);
 
     return new Promise((resolve, reject) => {
-        onSnapshot(query(IssueDBRef, where('created_at', '>=', lastWeekTimestamp)), (snapshot) => {
-            resolve(snapshot)
-        }, (err) => reject(err));
-    })
-}
+        onSnapshot(
+            query(IssueDBRef, where("created_at", ">=", lastWeekTimestamp)),
+            (snapshot) => {
+                resolve(snapshot);
+            },
+            (err) => reject(err)
+        );
+    });
+};
+
+export const issuesList = (issue_id) => {
+    return new Promise((resolve, reject) => {
+        onSnapshot(
+            query(
+                IssueDBRef,
+                orderBy("created_at", "desc"),
+                orderBy("latest_reported", "desc")
+            ),
+            (snapshot) => {
+                resolve(
+                    snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+                );
+            },
+            (err) => reject(err)
+        );
+    });
+};
+
+export const createIssueReport = async (issue_id, total_counter) => {
+    const issueRef = doc(IssueDBRef, issue_id);
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+
+    const reportsQuery = query(
+        IssueReportDBRef,
+        where("issue", "==", issueRef.path),
+        where("reported_date", "==", currentDate)
+    );
+    const reportsSnapshot = await getDocs(reportsQuery);
+
+    let response = null;
+
+    if (reportsSnapshot.size > 0) {
+        // If there is already a report for the same date, update the counter
+        const existingReport = reportsSnapshot.docs[0];
+        response = await updateDoc(doc(db, "IssueReport", existingReport.id), {
+            counter: existingReport.data().counter + 1,
+        });
+    } else {
+        // If no report exists for the date, create a new report
+        const newReport = {
+            issue: issueRef.path,
+            reported_date: currentDate,
+            counter: 1,
+        };
+        response = await addDoc(IssueReportDBRef, newReport);
+    }
+
+    if (response !== null) {
+        // update issue latest reported date
+        await updateDoc(issueRef, {
+            latest_reported: new Date(),
+            total_counter: total_counter + 1,
+        });
+    }
+    return response;
+};
+
+export const issueReportGraphData = async (issue_id) => {
+    const issueRef = doc(IssueDBRef, issue_id);
+
+    const reportsQuery = query(
+        IssueReportDBRef,
+        where("issue", "==", issueRef.path)
+        // orderBy('reported_date', "desc")
+    );
+    return await getDocs(reportsQuery).then((snapshot) => {
+        return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    });
+};
 
 export default IssueDBRef;
